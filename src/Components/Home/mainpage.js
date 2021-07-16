2c0cca1e94bc21fd12ae5979d6efa8e56425852f
jorgecm99 <jcorcoles86@hmail.com>
2021-07-16 11:09:04 +0200

    styles

--- src/Components/Home/mainpage.js
+++ src/Components/Home/mainpage.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
 
 const Mainimage = styled ('div')({
-    width: '860px',
-    textAlign: 'center'
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: '10px',
+    position: 'relative'
 })
 
 const Mainpicture = styled ('img')({
-    maxWidth: '100%'
+    maxWidth: '100%',
+
     
+})
+
+const Buttonstyle = styled ('button')({
+    border: '4px solid white',
+    borderRadius: '5px',
+    background: 'transparent',
+    color: 'white',
+    fontWeight: 'bold',
+    width: '100px',
+    margin: '650px -950px',
+    position: 'absolute'
+
 })
 
 const MainComponent = () => {
     return(
         <Mainimage>
             <Mainpicture src="https://www.aboutmanchester.co.uk/wp-content/uploads/2020/09/Shop.jpg" alt="outlet"></Mainpicture>
+            <Buttonstyle>
+                <Link to="/lista">
+                    SHOP
+                </Link>
+            </Buttonstyle> 
+            
         </Mainimage>
     )
 
